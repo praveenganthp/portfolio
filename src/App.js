e56eb59b1e25e2e5460250236547b0cfcc8952e1
praveenganth <vpraveenganth666@gmail.com>
2023-09-07 15:06:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Service from './components/Service';
-import Work from './components/Work';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Service from "./components/Service";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 // import ScrollToTopButton from './components/ScrollTopButton';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    
-    <div className='container-fluid'>
-      
-    <Navbar/>
-    <Home/>
-    <Service/>
-    <Work/>
-    <Contact/>
-    <Footer/>
-    
-    </div>
+    <BrowserRouter>
+      <div className="container-fluid">
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<Service />} />
+          <Route path="/about" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
